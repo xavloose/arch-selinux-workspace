49c7a593b1986180047a119f31581ca8140311d4
Xavier Loose <xav.loose@gmail.com>
2017-02-03 14:07:52 -0600

    added refpolicies directory

---
+++ scripts/firstTimeSetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "This Script is intended to be run once and work perfectly..."
+echo "It's unfortunately hardcoded for my system now... Generalizing it is on my TODO"
+
+git clone git@github.com:xavloose/selinux.git
+
+
+
